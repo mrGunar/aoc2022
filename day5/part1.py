6002b5af0702ffbdcb0804e9d2b282c8b08e7502
mrGunar <plotkin.andrei@gmail.com>
2022-12-05 09:02:57 +0300

    day5 part1

---
+++ day5/part1.py
@@ -0,0 +1,41 @@
+import re
+import dataclasses
+
+@dataclasses.dataclass
+class Data:
+    MOVE: int
+    FROM: int
+    TO: int
+
+#move from to
+
+structure = {
+    1: list("ZPMHR"),
+    2: list("PCJB"),
+    3: list("SNHGLCD"),
+    4: list("FTMDQSRL"),
+    5: list("FSPQBTZM"),
+    6: list("TFSZBG"),
+    7: list("NRV"),
+    8: list("PGLTDVCM"),
+    9: list("WQNJFML"),
+}
+
+def find(command: str):
+    c = map(int, re.findall("\d+", command))
+    return Data(*c)
+
+
+with open("day5/input.txt") as file:
+    commands = [x.rstrip("\n") for x in file.readlines()]
+
+for c in commands:
+    temp = find(c)
+    crates_from = structure[temp.FROM][:-temp.MOVE-1:-1]
+    structure[temp.FROM] = structure[temp.FROM][:-temp.MOVE]
+    structure[temp.TO].extend(crates_from)
+
+
+#print answer
+for k in structure.values():
+    print(k[-1], end="")
